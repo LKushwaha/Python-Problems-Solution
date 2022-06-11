973be2ecd9d71cc0232178a50c3f4a66ac59d434
LKushwaha <103353358+LKushwaha@users.noreply.github.com>
2022-06-11 09:51:26 +0530

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,1 +1,18 @@
+def fibonacci(n):          
+    first = 0              
+    second = 1             
+    list1 = []             
+    for i in range(n):     
+        third = first + sec
+        first = second     
+        second = third     
+        list1.append(third)
+        if third>n:        
+            break          
+    if n in list1:         
+        print("Yes")       
+           
+    else:                  
+        print("No")        
+                           
 
